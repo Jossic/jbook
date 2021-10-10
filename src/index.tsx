0711e3213ee193c24592b514d2d56d9ee29b039e
Jossic <jossic.lapierre@gmail.com>
2021-10-10 14:07:01 +0200

    modifs Mac

---
+++ src/index.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useRef, useState } from 'react';
+import ReactDom from 'react-dom';
+import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+
+const App = () => {
+	const [input, setInput] = useState('');
+	const [code, setCode] = useState('');
+	const ref = useRef<any>();
+
+	const startService = async () => {
+		ref.current = await esbuild.startService({
+			worker: true,
+			wasmURL: '/esbuild.wasm',
+		});
+	};
+
+	useEffect(() => {
+		startService();
+	}, []);
+
+	const onClick = async () => {
+		if (!ref.current) {
+			return;
+		}
+
+		const result = await ref.current.build({
+			entryPoints: ['index.js'],
+			bundle: true,
+			write: false,
+			plugins: [unpkgPathPlugin(input)],
+			define: {
+				'process.env.NODE_ENV': '"production"',
+				global: 'window',
+			},
+		});
+		// console.log('result =>', result);
+
+		setCode(result.outputFiles[0].text);
+	};
+	return (
+		<>
+			<textarea
+				value={input}
+				onChange={(e) => setInput(e.target.value)}></textarea>
+			<div>
+				<button onClick={onClick}>Valider</button>
+			</div>
+			<pre>{code}</pre>
+		</>
+	);
+};
+
+ReactDom.render(<App />, document.querySelector('#root'));
